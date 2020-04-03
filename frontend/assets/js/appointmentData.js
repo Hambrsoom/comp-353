8c3c6eff40ee65a475f3a6cb14874b43bbd3d499
Hambrsoom <hampico1234@gmail.com>
2020-04-03 15:11:54 -0400

    get all the appointments for a specific week

--- frontend/assets/js/appointmentData.js
+++ frontend/assets/js/appointmentData.js
@@ -22,9 +22,22 @@ function getAllAppointmentsForASpecificWeek() {
     
     const dentistId = document.getElementById('select-dentists').value;
     var date = document.getElementById('app-date-doctor').value;
-    console.log("date: "+date);
-    findTheDateForSunday(date);    
+    date = date.substring(6);
+    console.log(dentistId);
+    console.log(date);
+    
+    // beginningOfWeek = findTheDateForSunday(date);   
+    // endOfWeek = findTheDateForSaturday(date); 
 
+    xmlhttp.open(
+        'POST',
+        '../../Backend/Controllers/QueryControllers/GetAppointmentsForDoctor.php'
+    );
+    xmlhttp.setRequestHeader(
+        'Content-type',
+        'application/x-www-form-urlencoded'
+    );
+    
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById(
@@ -32,11 +45,9 @@ function getAllAppointmentsForASpecificWeek() {
             ).innerHTML = this.responseText;
         }
     };
-    xmlhttp.open(
-        'GET',
-        '../../Backend/Controllers/QueryControllers/GetAllAppointments.php',
-        true
-    );
+
+   
+
     xmlhttp.send( 
         '&date=' +
         date +
@@ -45,19 +56,35 @@ function getAllAppointmentsForASpecificWeek() {
 }
 
 function findTheDateForSunday(dateValue){
-    date1 = dateValue;
-    var d = new Date(date1);
+    var d = new Date(dateValue);
+
     d.setDate(d.getDate() +1);
+    console.log("getDate(): "+d.getDate());
+    console.log("getDay(): "+ d.getDay());
     console.log("date Today: "+ d);
     d.setDate(d.getDate()-d.getDay());
     console.log("date Sunday: "+ d); 
 
-    var dd = d.getDate();
-    var mm = d.getMonth()+1;
-    var yyyy = d.getFullYear();
-    beginningOfWeek = yyyy+'-'+mm+'-'+dd;
+    var month = d.getMonth()+1
+
+    beginningOfWeek = d.getFullYear()+'-'+month+'-'+d.getDate();
     console.log("begin: "+ beginningOfWeek);
     return beginningOfWeek;
+}
+
+function findTheDateForSaturday(dateValue){
+    console.log("Let's talk about sat now :");
+    var d = new Date(dateValue);
+    d.setDate(d.getDate() +1);
+    console.log("date Today: "+ d);
+    d.setDate(d.getDate()+(6-d.getDay()));
+    console.log("date Sat: "+ d); 
+    
+    var month = d.getMonth()+1
+
+    endOfWeek = d.getFullYear()+'-'+month+'-'+d.getDate();
+    console.log("begin: "+ endOfWeek);
+    return endOfWeek;
 }
 
 
