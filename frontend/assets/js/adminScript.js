076d5775326caad47578e3c55e390dff785add67
Lara <larat248@gmail.com>
2020-04-06 10:01:15 -0400

    Fixed queries

--- frontend/assets/js/adminScript.js
+++ frontend/assets/js/adminScript.js
@@ -1,5 +1,5 @@
 function executeStatement() {
-    const statement = document.getElementById('sql-statement').value.trim();
+    let statement = document.getElementById('sql-statement').value.trim();
     statement = sanitizeInput(statement);
     const statementType = statement
         .substring(0, statement.indexOf(' '))
--- frontend/assets/js/appointmentData.js
+++ frontend/assets/js/appointmentData.js
@@ -24,38 +24,7 @@ function onLoad() {
 // }
 
 
-// function getAllAppointmentsForASpecificWeek() {
-//     let xmlhttp = new XMLHttpRequest();
-    
-//     const dentistId = document.getElementById('select-dentists').value;
-//     var date = document.getElementById('app-date-doctor').value;
-//     date = date.substring(6);
-//     console.log(dentistId);
-//     console.log(date);
 
-//     xmlhttp.open(
-//         'POST',
-//         '../../Backend/Controllers/QueryControllers/GetAppointmentsForDoctor.php'
-//     );
-//     xmlhttp.setRequestHeader(
-//         'Content-type',
-//         'application/x-www-form-urlencoded'
-//     );
-
-//     xmlhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             document.getElementById(
-//                 'get-all-appointments-for-doctor'
-//             ).innerHTML = this.responseText;
-//         }
-//     };
-
-//     xmlhttp.send( 
-//         '&date=' +
-//         date +
-//         '&dentistId=' +
-//         dentistId);
-// }
 
 var dentistId = 0;
 
@@ -245,75 +214,6 @@ function getAppointmentDetailsByAppId(appId){
     };
     xmlhttp.send();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function findTheDateForSunday(dateValue){
-    var d = new Date(dateValue);
-
-    d.setDate(d.getDate() +1);
-    console.log("getDate(): "+d.getDate());
-    console.log("getDay(): "+ d.getDay());
-    console.log("date Today: "+ d);
-    d.setDate(d.getDate()-d.getDay());
-    console.log("date Sunday: "+ d); 
-
-    var month = d.getMonth()+1
-
-    beginningOfWeek = d.getFullYear()+'-'+month+'-'+d.getDate();
-    console.log("begin: "+ beginningOfWeek);
-    return beginningOfWeek;
-}
-
-function findTheDateForSaturday(dateValue){
-    console.log("Let's talk about sat now :");
-    var d = new Date(dateValue);
-    d.setDate(d.getDate() +1);
-    console.log("date Today: "+ d);
-    d.setDate(d.getDate()+(6-d.getDay()));
-    console.log("date Sat: "+ d); 
-    
-    var month = d.getMonth()+1
-
-    endOfWeek = d.getFullYear()+'-'+month+'-'+d.getDate();
-    console.log("begin: "+ endOfWeek);
-    return endOfWeek;
-}
-
-
-
-
-
-
-
-
 
 
 
