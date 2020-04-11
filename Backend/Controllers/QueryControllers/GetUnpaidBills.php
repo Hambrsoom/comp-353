b1c031b7a33e734a74fb0c54a0848858a824a3fa
Trevor N <trevor.nagg@gmail.com>
2020-04-11 15:30:43 -0400

    Alias for derived table was causing error due to capital letter in the name

--- Backend/Controllers/QueryControllers/GetUnpaidBills.php
+++ Backend/Controllers/QueryControllers/GetUnpaidBills.php
@@ -7,11 +7,11 @@
     appointment.date as `Date`, 
     appointment.time as `Time`, 
     concat(p.firstName,' ', p.lastName) as `Patient's Name`,
-    CostPerAppointment.totalCost as `Total Cost($)`
+    costPerAppointment.totalCost as `Total Cost($)`
     FROM bills as bills, appointments as appointment, patients as p, (select app.appointmentsID as appointmentID, SUM(t.cost) as totalCost
     from treatments as t, appointments as app, consist_of as treatmentsInApp
     where treatmentsInApp.treatment_id = t.treatmentID and app.appointmentsID = treatmentsInApp.appointment_id
-    group by app.appointmentsID) as CostPerAppointment
+    group by app.appointmentsID) as costPerAppointment
     WHERE  costPerAppointment.appointmentID = appointment.appointmentsID AND appointment.billID = bills.billID AND bills.paid=0 AND p.patientID = appointment.patientID;";    
 
     echo executeSelectStatement($sql);
